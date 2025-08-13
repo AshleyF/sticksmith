a0ed7fa742ebf33bb5baee52ed4c57c5a40c6d97
Ashley Feniello <ashleyf@microsoft.com>
2025-08-13 12:56:25 -0700

    3-piezo trigger

--- trigger/trigger.ino
+++ trigger/trigger.ino
@@ -1,87 +1,214 @@
 //#define DEBUG
+#define PULLDATA
 
-const int adcPin = 28;  // GPIO 28 = physical pin 34 = ADC2
+const int adcPinA = 26; // GPIO 26 = physical pin 31 = ADC0
+const int adcPinB = 27; // GPIO 27 = physical pin 32 = ADC1
+const int adcPinC = 28; // GPIO 28 = physical pin 34 = ADC2
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);  // Wait for USB serial on Pico W
 }
 
-const double GAIN = 5.0;
-const int THRESHOLD = 25 * GAIN; // observed 110
+const double GAIN = 4.0;
+const int THRESHOLD = 50;
 const double MAX = 1023;
 const double RANGE = MAX - THRESHOLD;
-const unsigned long DELAY = 30000; // observed 1.1-1.7ms
-const unsigned long MASK = 60000; // observed 500-700ms
+const unsigned long DELAY = 2000; // observed 600-750
+const unsigned long MASK = 1000000; // observed ~800
 
-int trigger(int value, unsigned long time, unsigned long *hitTime, unsigned long *peakTime, int *peak, int *mode)
+int trigger(int value, unsigned long time, unsigned long *hitTime, unsigned long *peakTime, int *peak, int *state)
 {
-  if (*mode < 2 && value > *peak)
+  int rtn = 0;
+  //Serial.print(value); Serial.print(' ');
+  if (*state < 2 && value > *peak)
   {
     *peak = value;
     *peakTime = time;
-    if (*mode == 0 /* waiting for hit */)
+    if (*state == 0 /* waiting for hit */)
     {
       *hitTime = time;
-      *mode = 1; // delay for peak
+      *state = 1; // delay for peak
+#ifdef DEBUG
+      Serial.println(" state=1");
+      Serial.flush();
+#endif
     }
   }
-  if (*mode > 0)
+  if (*state > 0)
   {
     unsigned long elapsed = time - *hitTime;
-    if (*mode == 1 /* delay for peak*/ && elapsed > DELAY)
+    if (*state == 1 /* delay for peak*/ && elapsed > DELAY)
     {
 #ifdef DEBUG
       Serial.print("hit ");
       Serial.print(*peak);
       Serial.print(" peak: ");
       Serial.print(*peakTime - *hitTime);
+      Serial.flush();
 #endif
       int hit = *peak;
       *peak = THRESHOLD;
-      *mode = 2 /* wait for decay*/;
-      return hit;
+      *state = 2 /* wait for decay*/;
+#ifdef DEBUG
+      Serial.println(" state=2");
+      Serial.flush();
+#endif
+      rtn = hit;
+      //return hit;
     }
-    if (*mode == 2 && value < THRESHOLD)
+#ifdef DEBUG
+    if (*state == 2)
+    {
+      Serial.print(value);
+      Serial.print(' ');
+      Serial.print('.');
+      Serial.flush();
+      delay(100);
+    }
+#endif
+    if (*state == 2 && value < THRESHOLD)
     {
 #ifdef DEBUG
       Serial.print(" decay: ");
       Serial.println(time - *hitTime);
+      Serial.flush();
 #endif
-      *mode = 3; // wait for mask time
+      *state = 3; // wait for mask time
+#ifdef DEBUG
+      Serial.println(" state=3");
+      Serial.flush();
+#endif
+
     }
-    if (*mode == 3 /* mask for decay*/ && elapsed > MASK)
+    if (*state == 3 /* mask for decay*/ && elapsed > MASK)
     {
-      *mode = 0; // waiting for hit
+      *state = 4; // idle
+#ifdef DEBUG
+      Serial.println(" state=0");
+      Serial.flush();
+#endif
     }
   }
-  return 0;
+  return rtn;
 }
 
-unsigned long hitTime0;
-unsigned long peakTime0;
+unsigned long hitTimeA = 0;
+unsigned long hitTimeB = 0;
+unsigned long hitTimeC = 0;
+unsigned long peakTimeA;
+unsigned long peakTimeB;
+unsigned long peakTimeC;
 
-int peak0 = THRESHOLD;
-int mode0 = 0; // waiting for hit
+int peakA = THRESHOLD;
+int peakB = THRESHOLD;
+int peakC = THRESHOLD;
+int stateA = 0; // waiting for hit
+int stateB = 0; // waiting for hit
+int stateC = 0; // waiting for hit
 
+int lastHitA = 0;
+int lastHitB = 0;
+int lastHitC = 0;
+
+int loopMode = 0; // waiting for hit
+
+double score(int refA, int refB, int refC, int timeA, int timeB, int timeC)
+{
+  double a = (refA - timeA);
+  double b = (refB - timeB);
+  double c = (refC - timeC);
+  return sqrt(a * a + b * b + c * c);
+}
+
+#ifdef PULLDATA
 void loop() {
-  int value = analogRead(adcPin) * GAIN;  // Read from ADC2 (GPIO 28)
-  //Serial.println(value);
-  unsigned long time = micros();
-  int hit = trigger(value, time, &hitTime0, &peakTime0, &peak0, &mode0);
-  if (hit > 0)
+  int hitA = analogRead(adcPinA);
+  int hitB = analogRead(adcPinB);
+  int hitC = analogRead(adcPinC);
+  Serial.write(255);
+  Serial.write((byte)min(254, hitA));
+  Serial.write((byte)min(254, hitB));
+  Serial.write((byte)min(254, hitC));
+  Serial.flush();
+  yield();
+  delay(1);
+}
+#else
+void loop() {
+  unsigned long now = micros();
+  int hitA = trigger(analogRead(adcPinA), now, &hitTimeA, &peakTimeA, &peakA, &stateA);
+  int hitB = trigger(analogRead(adcPinB), now, &hitTimeB, &peakTimeB, &peakB, &stateB);
+  int hitC = trigger(analogRead(adcPinC), now, &hitTimeC, &peakTimeC, &peakC, &stateC);
+  delay(100);
+  if (loopMode == 0) // waiting for hit
   {
-    byte velocity = ((double)(hit - THRESHOLD) * 255 / RANGE) + 0.5; // 1 - 127
-    Serial.write(velocity);
-    Serial.flush();
-    yield();
+    if (hitA > 0) { lastHitA = hitA; }
+    if (hitB > 0) { lastHitB = hitB; }
+    if (hitC > 0) { lastHitC = hitC; }
+    if (lastHitA > 0 && lastHitB > 0 && lastHitC > 0)
+    {
+      unsigned long first = min(hitTimeA, min(hitTimeB, hitTimeC));
+      int tA = hitTimeA - first;
+      int tB = hitTimeB - first;
+      int tC = hitTimeC - first;
+      Serial.print("A: ");
+      Serial.print(tA);
+      Serial.print(" B: ");
+      Serial.print(tB);
+      Serial.print(" C: ");
+      Serial.println(tC);
+
+      double center = score(0, 40, 32000, tA, tB, tC);
+      double left = score(2090, 0, 1000, tA, tB, tC);
+      double right = score(250, 0, 29000, tA, tB, tC);
+
+      Serial.print("Center: ");
+      Serial.println(center);
+      Serial.print("Left: ");
+      Serial.println(left);
+      Serial.print("Right: ");
+      Serial.println(right);
+      
+
+      //if (diffA - diffB < 1000)
+      //{
+      //  //Serial.println("LEFT");
+      //  Serial.write((byte)0);
+      //}
+      //else
+      //{
+      //  //Serial.println("RIGHT");
+      //  Serial.write((byte)1);
+      //}
+      //Serial.println("----------------------------------------------------------");
+      //Serial.flush();
+  #ifndef DEBUG
+      int vel = max(max(lastHitA, lastHitB), lastHitC);
+      //Serial.write(((double)min((vel - THRESHOLD) * GAIN, MAX) * 254 / RANGE) + 1.5); // 1 - 127
+      Serial.flush();
+      yield();
+  #endif
+      loopMode = 1; // wait for idle
+    }
+  }
+  else if (loopMode == 1 /* wait for idle */ && stateA == 4 && stateB == 4 && stateC == 4) // all idle?
+  {
+    // reset everything for good measure
+    lastHitA = lastHitB = lastHitC = 0;
+    hitTimeA = hitTimeB = hitTimeC = 0;
+    peakA = peakB = peakC = THRESHOLD;
+    stateA = stateB = stateC = 0;
+    lastHitA = lastHitB = lastHitC = 0;
+    loopMode = 0; // back to waiting for hit
   }
 }
+#endif // PULLDATA
 
 /*
-ADC0	GP26	Pin 31
-ADC1	GP27	Pin 32
-ADC2	GP28	Pin 34
+ADC0	GP26	Pin 31 A
+ADC1	GP27	Pin 32 B
+ADC2	GP28	Pin 34 C
 
 3	GND
 8	GND
@@ -103,4 +230,5 @@ ADC2	GP28	Pin 34
           GND
 
 https://my.eng.utah.edu/~cs5789/handouts/piezo.pdf
+https://forum.arduino.cc/t/how-to-wire-a-piezo-as-an-impact-sensor/901726
 */
